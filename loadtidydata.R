19e7da1cfb57c83ab6a84b0f8e07fbd4e3e616da
bhaskar-reddy <kvmbhaskar@gmail.com>
2014-08-09 20:07:50 -0700

    Load 1st plot and its dependency source file

---
+++ loadtidydata.R
@@ -0,0 +1,51 @@
+loadtidydata <- function() {
+        
+        ## if file does not exist, download file from the cloudfront site
+        zipfile <- file.path(getwd(), "household_power_consumption.zip")
+        if (!file.exists(zipfile)) {
+                url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+                
+                download.file(url, zipfile, method="curl")
+                dateDownloaded <- date()
+        
+                ## unzip file, saves as 'household_power_consumption.txt'
+                f <- unzip(zipfile)
+        } else {
+                ## if file exists, get file path
+                f <- file.path(getwd(), "household_power_consumption.txt")
+        }
+
+        set.seed(10)
+
+        ## to get a subset of the data based on dates 2007-02-01 and 2007-02-02 
+        ## using read.table, we need to 
+        ## 1. store columns names in a temp variable
+        ## 2. calculate number of rows to skip, so that 1st row starts from 2007-02-01
+        ## 3. calculate number of rows to read, so that final row ends on 2007-02-02
+
+        ## 1. to store column names, create tmp table, with just 1 row
+        tmp <- read.table(f, sep=";", header=T, nrow=1)
+        columns <- colnames(tmp)
+        
+        ## 2. calculate number of rows to skip, based on first row information
+        sttime = strptime('16/12/2006 17:24:00',format='%d/%m/%Y %H:%M:%S')
+        endtime <- strptime('01/02/2007 00:00:00',format='%d/%m/%Y %H:%M:%S')
+        dftime <- difftime(endtime, sttime, units="mins")
+        skiprows <- as.numeric(dftime)
+        
+        ## 3. calculate number of rows to read, based on date below 2007-02-03
+        sttime = strptime('01/02/2007 00:00:00',format='%d/%m/%Y %H:%M:%S')
+        endtime <- strptime('03/02/2007 00:00:00',format='%d/%m/%Y %H:%M:%S')
+        dftime <- difftime(endtime, sttime, units="mins")
+        numrows <- as.numeric(dftime)
+
+        ## load data filtered by the date that is of interest to us.
+        df <- read.table(f, sep=";", header=T, skip=skiprows, nrow=numrows, 
+                         na.strings="?")
+        
+        ## set the column names back
+        names(df) <- columns
+
+        ## return the data.frame
+        df
+}
---
+++ plot1.R
@@ -0,0 +1,18 @@
+## Common code used for all the plots is contained a single common file.
+## This file is named "loadtidaydata.R". It performs the following common 
+## functions :
+##      1. Download data ("household_power_consumption.zip") from cloudfront site
+##      2. Unzip as "household_power_consumption.txt" file
+##      3. Load the filtered data for dates '2007-02-01' and '2007-02-02'
+
+## source "loadtidydata.R"
+source("loadtidydata.R")
+
+## call loadtidydata() function
+df <- loadtidydata()
+
+## plot the first png
+png("plot1.png", width = 480, height = 480, units = "px")
+hist(df$Global_active_power, col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
+dev.off()
