cec8b9f9eaf672d40ccba6c8fe4145978ed39b32
bhaskar-reddy <kvmbhaskar@gmail.com>
2014-08-09 21:21:07 -0700

    Plot 2 source and PNG file

---
+++ plot2.R
@@ -0,0 +1,20 @@
+## Common code used for all the plots is contained a single common file named
+## "loadtidydata.R". It performs the following common functions :
+##      1. Download data ("household_power_consumption.zip") from cloudfront site
+##      2. Unzip as "household_power_consumption.txt" file
+##      3. Load the filtered data for dates '2007-02-01' and '2007-02-02'
+
+## source "loadtidydata.R"
+source("loadtidydata.R")
+
+## call loadtidydata() function
+df <- loadtidydata()
+
+## Add new DateTime variable concatenating Date and Time provided
+df$DateTime <- strptime(paste(df$Date,df$Time), "%d/%m/%Y %H:%M:%S")
+
+## plot the second png
+png("plot2.png", width = 480, height = 480, units = "px")
+plot (df$DateTime, df$Global_active_power, type="l", xlab="",
+      ylab="Global Active Power (kilowatts)")
+dev.off()
